554f17e53195bcd0fd824cc6551d9fa96bbe3aa4
markjuvert <60376215+markjuvert@users.noreply.github.com>
2022-01-15 05:19:56 -0600

    modified content

--- backend/api/GetVisitCounter.cs
+++ backend/api/GetVisitCounter.cs
@@ -22,7 +22,7 @@ namespace Company.Function
             [CosmosDB(databaseName:"MyResumeDB", collectionName:"Counter", ConnectionStringSetting = "MyResumeDBConnectionString", Id = "1", PartitionKey = "1")] out Counter UpdatedCounter,
             ILogger log)
         {
-            // Here is where the counter gets updated that appears on the index
+            // Here is where the counter gets updated that appears on the index!!!
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             UpdatedCounter = counter;
